b8f90b5e2cf0fb349b391413e75ffaab74769a93
TI-IT <go.TIIT.ogle@gmail.com>
2020-06-13 11:36:39 +0300

    20

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,25 +1,26 @@
 import React from 'react';
 import s from './Navbar.module.css';
+import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
     return (
-        <nav className={s.nav}>
-            <div className={s.item}>
-                <a href='#3'>Profile</a>
-            </div>
-            <div className={`${s.item} ${s.activ}`}>
-                <a href='#3'>Messages</a>
-            </div>
-            <div className={s.item}>
-                <a href='#3'>News</a>
-            </div>
-            <div className={s.item}>
-                <a href='#3'>Music</a>
-            </div>
-            <div className={s.item}>
-                <a href='#3'>Settings</a>
-            </div>
-        </nav>
+      <nav className={s.nav}>
+        <div className={s.item}>
+          <NavLink to="/profile" activeClassName={s.activlink}>Profile</NavLink>
+        </div>
+        <div className={`${s.item} ${s.activ}`}>
+          <NavLink to="/dialogs" activeClassName={s.activlink}>Messages</NavLink>
+        </div>
+        <div className={s.item}>
+          <aNavLink to="/news" activeClassName={s.activlink}>News</aNavLink>
+        </div>
+        <div className={s.item}>
+          <NavLink to="/music" activeClassName={s.activlink}>Music</NavLink>
+        </div>
+        <div className={s.item}>
+          <NavLink to="settings" activeClassName={s.activlink}>Settings</NavLink>
+        </div>
+      </nav>
     );
 }
 
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -4,8 +4,8 @@ import MyPosts from './MyPosts/MyPosts';
 
 
 const Profile = () => {
-    return <div className={s.content}>
-            <div>
+    return <div>
+            <div className={s.content}>
                 <img src='https://wallbox.ru/wallpapers/main/201330/2b81c266a9de284.jpg'></img>
             </div>
             <div>
